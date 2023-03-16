8e75da49913bb3068534d2dc2d1b110ad751d3a6
leosant <leonardosantos45548@gmail.com>
2023-03-16 01:47:48 -0300

    Used localstorage to persist data

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -8,7 +8,25 @@ import { TaskList } from '../../model/task-list';
   styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent implements DoCheck {
+  public taskList: Array<TaskList> = JSON.parse(
+    localStorage.getItem('list') || '[]'
+  );
+
+  constructor() {}
+
   ngDoCheck(): void {
+    if (!this.taskList) {
+      return;
+    }
+    this.sortTaskList();
+    this.setLocalStorage();
+  }
+
+  public setLocalStorage(): void {
+    localStorage.setItem('list', JSON.stringify(this.taskList));
+  }
+
+  public sortTaskList(): void {
     this.taskList.sort(
       (first, last) => Number(first.checked) - Number(last.checked)
     );
@@ -16,9 +34,6 @@ export class TodoListComponent implements DoCheck {
   setTaskToList(event: string) {
     this.taskList.push({ task: event, checked: false });
   }
-  public taskList: Array<TaskList> = [];
-
-  constructor() {}
 
   public deleteItemTaskList(index: number): void {
     this.taskList.splice(index, 1);
